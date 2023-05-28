5ec53bb9d47c5189b8079b83c6ee1eec944a52e4
Lemonade7 <wmaglangit1118@gmail.com>
2023-05-28 18:32:49 +0800

    changed implementation, due to depreciated extension

--- app/src/main/java/com/redwaltz/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/redwaltz/calculatorapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.redwaltz.calculatorapp.databinding.ActivityMainBinding
 import com.redwaltz.calculatorapp.ui.theme.CalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,9 @@ class MainActivity : ComponentActivity() {
     private var canAddDecimal = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent(R.layout.activity_main)
+
+        val view = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(view.root)
     }
 
     fun numberAction(view: View) {
