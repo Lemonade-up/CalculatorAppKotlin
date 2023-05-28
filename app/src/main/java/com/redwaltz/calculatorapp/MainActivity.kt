2adc1f455964ea903644e514b6d76f54a761c390
Lemonade7 <wmaglangit1118@gmail.com>
2023-05-28 20:39:37 +0800

    added multiple onlclick listeners

--- app/src/main/java/com/redwaltz/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/redwaltz/calculatorapp/MainActivity.kt
@@ -15,8 +15,6 @@ import com.redwaltz.calculatorapp.ui.theme.CalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
 
-    private var canAddOperation = false
-    private var canAddDecimal = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -33,9 +31,61 @@ class MainActivity : ComponentActivity() {
             if (length > 0)
                 binding.inputTV.text = binding.inputTV.text.subSequence(0, length-1)
         }
+        binding.button0.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"0")
+        }
+        binding.button1.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"1")
+        }
+        binding.button2.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"2")
+        }
+        binding.button3.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"3")
+        }
+        binding.button4.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"4")
+        }
+        binding.button5.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"5")
+        }
+        binding.button6.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"6")
+        }
+        binding.button7.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"7")
+        }
+        binding.button8.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"8")
+        }
+        binding.button9.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"9")
+        }
+        binding.buttonPeriod.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,".")
+        }
+
+        binding.addButton.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"+")
+        }
+        binding.minusButton.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"-")
+        }
+        binding.multiButton.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"×")
+        }
+        binding.divideButton.setOnClickListener{
+            binding.inputTV.text = addToInputText(binding,"÷")
+        }
+        binding.equalButton.setOnClickListener{
+            showResult()
+        }
+
 
     }
 
-
+    private fun addToInputText(binding: ActivityMainBinding,buttonValue: String): String {
+        return "${binding.inputTV.text}$buttonValue"
+    }
 
 }
